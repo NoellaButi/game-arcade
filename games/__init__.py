0881f7bf837d71ab993041cb69cdd9d1c0b0f3e6
Noella Buti <noellabuti@gmail.com>
2025-09-21 23:22:10 -0700

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,4 +1,17 @@
 # __init__.py
 
-from . import (dice_roller, nine_lives, mboka_quiz, slot_machine, number_guess,
-               rps, robot_builder, tictactoe, memory, blackjack)
+from . import dice_roller as dice_roller
+from . import nine_lives as nine_lives
+from . import mboka_quiz as mboka_quiz
+from . import slot_machine as slot_machine
+from . import number_guess as number_guess
+from . import rps as rps
+from . import robot_builder as robot_builder
+from . import tictactoe as tictactoe
+from . import memory as memory
+from . import blackjack as blackjack
+
+__all__ = [
+    "dice_roller", "nine_lives", "mboka_quiz", "slot_machine",
+    "number_guess", "rps", "robot_builder", "tictactoe", "memory", "blackjack",
+]
